cce5d38d981e756ef4e583915f9125c8e64f36fd
Cesscoo <martinhernandez.fj@gmail.com>
2022-09-22 08:52:25 +0000

    Completado - se generan excusas aleatorias

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excusa").innerHTML = excusas();
+  console.log("Se generan excusas aleatorias!");
 };
+
+function excusas() {
+  let who = ["The dog", "My granma", "His turtle", "My bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  return (
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)]
+  );
+}
